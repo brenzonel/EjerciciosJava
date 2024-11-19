b6bd63a45a27fdec592c39276c07d6a6efb2b6ba
brantest174 <bran.test27@gmail.com>
2024-11-18 21:42:25 -0600

    Add: Valores de i++ y ++i

---
+++ Otros/pruebas.java
@@ -0,0 +1,13 @@
+class pruebas {
+	public static void main (String args[]){
+	int i=1, j=1;
+	System.out.println("i es: "+i);
+	System.out.println("j es: "+j);
+	i=++j;
+	System.out.println("i es: "+i);
+	System.out.println("j es: "+j);
+	j=i++;
+	System.out.println("i es: "+i);
+	System.out.println("j es: "+j);
+	}
+}
